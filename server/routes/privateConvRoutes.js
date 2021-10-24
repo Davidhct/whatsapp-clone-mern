e08e63d2b4142fe95a72ee57d1dee308061c2c8e
Davidhct <davidhct1@gmail.com>
2021-10-24 16:19:36 +0300

    create privte conversation controller, routes and model

---
+++ server/routes/privateConvRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const privateConvController = require('../controllers/privateConvController');
+const router = express.Router();
+
+router
+  .route('/')
+  .get(privateConvController.getAllMessages)
+  .post(privateConvController.createMessage)
+  .patch(privateConvController.updateMesssages);
+
+router.route('/:userId').get(privateConvController.getMessage);
+
+module.exports = router;
